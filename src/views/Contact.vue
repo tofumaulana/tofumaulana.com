c5c075f32922c4cd88a76c8136ed9fb446e7a346
tofumaulana <161713454+tofumaulana@users.noreply.github.com>
2024-05-22 14:08:57 +0700

    remov contac

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,26 +1,26 @@
 <template>
-  <form class="max-w-sm md:max-w-xl mx-auto py-14 px-5">
+  <form class="max-w-sm md:max-w-xl mx-auto py-20 px-5">
     <div class="text-center">
       <h1 class="text-4xl font-bold">Hubungi Kami</h1>
     </div>
     <div class="">
-      <div class="py-3">
-        <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+      <div class="py-3 mt-5">
+        <label for="text" class="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
         <input
           type="text"
           id="text"
-          class="shadow-sm bg-slate-700 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          class="shadow-sm bg-slate-700 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           placeholder="Full Name"
           required
         />
       </div>
       <div class="py-3">
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
         <input
           type="email"
           id="email"
           placeholder="Email"
-          class="shadow-sm bg-slate-700 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          class="shadow-sm bg-slate-700 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           required
           v-model="email"
         />
@@ -43,7 +43,7 @@
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Kirim
+        Send
       </button>
     </div>
   </form>
